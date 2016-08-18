0e20d4f7fc714eca180f3eb577ac8237252b7e08
anbumanid <newanbu22@yahoo.com>
2016-08-18 16:19:26 +0530

    Initial commit

---
+++ SingleViewApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewApp
+//
+//  Created by Anbumani on 18/08/16.
+//  Copyright © 2016 HCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewApp
+//
+//  Created by Anbumani on 18/08/16.
+//  Copyright © 2016 HCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
